5713072daa83073b53ddc511276d3e7eae92e51a
shirobite <kibromh45@gmail.com>
2022-04-08 02:24:12 +0300

    print in c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - entry point
- *
- * RETURN: always 0 (sucess)
+ * main - main block
+ * Return: 0
  */
-int main (void)
+int main(void)
 {
-	puts("\"programming is like building a multilingual puzzel");
-	print (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
+
