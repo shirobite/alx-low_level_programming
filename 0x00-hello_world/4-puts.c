aabbb88472f87b9dfd75782db69182e008ae3f8c
shirobite <kibromh45@gmail.com>
2022-04-08 02:10:54 +0300

    simple print in c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * RETURN: always 0 (sucess)
+ */
+int main (void)
+{
+	puts("\"programming is like building a multilingual puzzel");
+	print (0);
