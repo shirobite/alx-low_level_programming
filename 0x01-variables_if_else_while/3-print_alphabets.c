69d7c68ed3e4cf0add9cefffc41fbd042ade7c00
shirobite <kibromh45@gmail.com>
2022-04-11 00:16:23 +0300

    go

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,11 +7,11 @@
  * Description: print the lowercase alphabet
  * Return: 0
  */
-int main(void)
+	int main(void)
 {
 	int n;
-	srand(time(0));
 
+	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	for (n = 0; n < 26; ++n)
 	putchar('a' + n);
