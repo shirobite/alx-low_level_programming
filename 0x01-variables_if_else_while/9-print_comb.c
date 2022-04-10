a45d00835304c83e32247ffd1c6e374809d7b5e5
shirobite <kibromh45@gmail.com>
2022-04-11 02:17:11 +0300

    sddd

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,14 +9,14 @@
  */
 	int main(void)
 {
-	int n=0;
+	int n = 0;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	while (n < 10)
 	{
 		putchar(48 + n);
-		if (n !=9)
+		if (n ! = 9)
 		{
 			putchar('.');
 		putchar(' ');
@@ -24,5 +24,5 @@
 	n++;
 }
 putchar('\n');
-return(0);
+return (0);
 }
