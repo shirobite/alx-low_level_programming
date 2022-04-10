683f3c5827c5bb934fca8cdc83e0fd987088bc2c
shirobite <kibromh45@gmail.com>
2022-04-10 23:39:07 +0300

    my commit

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print the lowercase alphabet
+ * Return: 0
+ * /
+ int main(void)
+ {
+ 	int n;
+	strand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 0; c < 26; ++c)
+	putchar('a' + c);
+	for (c = 0; c < 26; ++c)
+	putchar('A' + c);
+	putchar('\n');
+	return (0);
+	}
