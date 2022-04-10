837edde8be6a24d1e388145abdbfb5bb90dc5504
shirobite <kibromh45@gmail.com>
2022-04-11 00:40:54 +0300

    commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: print the lowercase alphabet
+ * Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 'a'; n <= 'z'; ++n)
+	{
+		if (n != 'e' && n != 'q')
+			putchar(n);
+	}
+	putchar('\n');
+	return (0):
+}
