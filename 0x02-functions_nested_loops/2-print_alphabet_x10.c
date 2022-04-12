5f4e2f964f547d4b6fc144b22bb373de118598bf
shirobite <kibromh45@gmail.com>
2022-04-13 01:38:19 +0300

    letter 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ *print_alphabet_x10 - prints the alphabet 10x times
+ **/
+void print_alphabet_x10(void)
+{
+	int i;
+	for (i = 0; i <= 3; i++)
+	{ char c;
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			putchar(c);
+		}
+		putchar('\n');
+	}
