8d651cc4cd76dd6cb9aa9a27fdb7e5b544761186
shirobite <kibromh45@gmail.com>
2022-04-12 23:17:30 +0300

    print putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point the lowercase alphabet
+ *
+ * Return: 0
+ */
+ int main(void)
+{
+	putchar("_putchar\n");
+}
+
+
+
