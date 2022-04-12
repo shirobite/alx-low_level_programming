8257a89e275b28f5ba47dc3b1c6f8a41a11ea0db
shirobite <kibromh45@gmail.com>
2022-04-13 00:43:03 +0300

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,7 @@
  int main(void)
  {
  	int i;
- 	alphabet[] = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+ 	char alphabet[] = "abcdefghijklmnopqrstuvwxyz";
 	for (i = 0;i < 28; i++)
 	{
 	putchar(alphabet[i]);
