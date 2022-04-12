7e5f25d5735d5c1c5c2a64762aea9002d358edfc
shirobite <kibromh45@gmail.com>
2022-04-13 00:32:11 +0300

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ * /
+ int main(void)
+ {
+ 	int i;
+ 	alphabet[] = [a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z];
+	for (i = 0;i < 28; i++)
+	{
+	putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+	}
