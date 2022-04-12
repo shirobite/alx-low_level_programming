bdcee3ba6fd3bd5fd71cc4ddef1e3266ca190b6d
shirobite <kibromh45@gmail.com>
2022-04-13 02:45:23 +0300

    check float

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * print_sign - print a sign of a number
+ * @n: sign number
+ * Return:1 id n is great 0 if n is zero -1if n less than zero
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{ 
+		putchar("+");
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar("0");
+		return (0);
+	}
+		else
+		{ 
+			putchar("-");
+			return (-1);
+		}
+	}
+
