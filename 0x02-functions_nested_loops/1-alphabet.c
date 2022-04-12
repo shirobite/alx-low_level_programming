dd905419c030919dc86c4bb76f7dfae22a870156
shirobite <kibromh45@gmail.com>
2022-04-13 01:20:08 +0300

    letter

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,14 +2,13 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * print_alphabet - prints alphabet in lower followed by a new line lowercase alphabet
- *
- **/
+*print_alphabet - prints alphabet in lowercase followed by a new line 
+*
+**/
 void print_alphabet(void)
 {
 char i;
- 	
 for (i = 'a'; i <= 'z'; i++)
-	putchar(i);
+putchar(i);
 putchar('\n');
 }
