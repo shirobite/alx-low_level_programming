e2b09f7207a6692a50446ff24d994e12942c2e7e
shirobite <kibromh45@gmail.com>
2022-04-19 00:26:36 +0300

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,17 +1,16 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * _puts - prints string
- * @str: input string
- * Return: no return
+ *_puts - prints a string
+ * @str: string to print
+ *
+ * Description: prints a string
+ * On success: return the number of characters printed
  */
 
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{
-		_putchar(*str);
-		str++;
-	}
-	_putchar('\n');
+while (*str)
+_putchar(*str++);
+
+_putchar('\n');
 }
