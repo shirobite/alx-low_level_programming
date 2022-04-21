74506ed5898e8729afd607e9a1457016bb69e36d
shirobite <kibromh45@gmail.com>
2022-04-22 00:11:06 +0300

    3 digit

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,18 +11,18 @@ int main(void)
 	int j;
 	int k;
 
-	for (i == 0; i <= 9; i++)
+	for (i = 0; i <= 9; i++)
 	{
-		for (j == 0; j<= 9; j++)
+		for (j = 0; j<= 9; j++)
 		{
-			for (k == 0; k<= 9; k++)
+			for (k = 0; k<= 9; k++)
 			{
 				if (i < j || j < k)
 				{
 					putchar(i);
 			        	putchar(j);
-					putcahr(k);
-					if (c != '7')
+					putchar(k);
+					if (i != '7')
 					{
 						putchar(',');
 						putchar(' ');
