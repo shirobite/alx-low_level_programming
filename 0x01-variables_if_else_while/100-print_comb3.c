d5b3e50992253c3af6e77ebd8fac004858e2eef9
shirobite <kibromh45@gmail.com>
2022-04-21 23:38:04 +0300

    two digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -9,9 +9,10 @@ int main(void)
 {
 	int a;
 	int b;
-	for(a = 0; a <= 9; a++)
+
+	for (a = 0; a <= 9; a++)
 	{
-		for(b = 0; b <= 9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			if (!(a == b) && a < b)
 			{
