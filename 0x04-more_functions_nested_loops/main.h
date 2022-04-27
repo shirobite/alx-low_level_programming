9cc2c235fa8dc024307a9f36ae79511a82b36c36
shirobite <kibromh45@gmail.com>
2022-04-28 00:15:44 +0300

    upper case check

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,10 +1,10 @@
-#ifndef main.h
-#define main.h
-
-int _putchar(char c);
-int _isupper(int c);
-int _isdigit(int c);
-int mul(int a, int b);
+#ifndef MAIN_H
+#define MAIN_H
+/**
+ * *main - header file for protoypes
+ * *void protoypes (void)
+ * *int protoypes (int)
+ * */
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
@@ -13,5 +13,9 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
+int _putchar(char);
+int _isupper(int);
+int _isdigit(int);
+int mul(int, int);
 
-#endif
+#endif /* MAIN_H */
