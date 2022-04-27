a49b48a01a9a724ffc7a50c5ceec73d0c2041043
shirobite <kibromh45@gmail.com>
2022-04-28 00:52:44 +0300

    print most number

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,19 +1,17 @@
 #include "main.h"
 /**
-*print_numbers - function that print all numbers from
+*print_most_numbers - function that print most numbers from
 *0 to 9
 *
 *Return: returns nothing
 */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 int number = 48;
 for (number = 48; number < 58; number++)
 {
-	if (number != 49 && number !=57)
-		continue:
-	else 
-		break:
+	if ((number == 50) && (number == 52))
+		continue;
 _putchar(number);
 }
 _putchar(10);
