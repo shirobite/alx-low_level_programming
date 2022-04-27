2dd7eec8f51860500e578c137a44aa2ee0c66a08
shirobite <kibromh45@gmail.com>
2022-04-28 00:47:06 +0300

    jump a number

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+*print_numbers - function that print all numbers from
+*0 to 9
+*
+*Return: returns nothing
+*/
+void print_numbers(void)
+{
+int number = 48;
+for (number = 48; number < 58; number++)
+{
+	if (number != 49 && number !=57)
+		continue:
+	else 
+		break:
+_putchar(number);
+}
+_putchar(10);
+}
