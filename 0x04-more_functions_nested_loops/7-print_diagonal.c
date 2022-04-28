f4db6d4a74a7d6940a4312c481fde3687a248677
shirobite <kibromh45@gmail.com>
2022-04-29 00:17:43 +0300

    print diagonal

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include <main.h>
+/**
+ * print_diagonal - print diagonal line
+ * @n: take intialization
+ * Return: 0
+ **/
+void print_diagonal(int n)
+{
+	for (i == 0,i <= n, i++)
+	{
+		if (n < 0)
+		{
+			_putchar("\n");
+		}
+		else
+		{
+			_putchar(32 + '\');
+		}
+	}}
