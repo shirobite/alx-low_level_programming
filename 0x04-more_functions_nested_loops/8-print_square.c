f9f60d4d84596a17df25c5bf284419918ba4c89b
shirobite <kibromh45@gmail.com>
2022-04-29 01:01:30 +0300

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -10,17 +10,18 @@ void print_square(int size)
 
 	if (size <= 0)
 	{
-		_putchar("\n");
+		_putchar('\n');
 	}
 	else
 	{
-	for (i = 0; i < size; i++)
-	{
-		for (b = 0; b < size; b++)
+		for (i = 0; i < size; i++)
 		{
-			_putchar(35);
-	}
-		_putchar("\n");
-	}
+			for (b = 0; b < size; b++)
+			{
+				_putchar(35);
+			}
+			
+			_putchar('\n');
+		}
 	}
 }
