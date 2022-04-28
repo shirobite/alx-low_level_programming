c460d8e40cd727d7711df2049675cd65afcf4579
shirobite <kibromh45@gmail.com>
2022-04-28 15:39:55 +0300

    print 14

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * more_numbers - prints the numbers 0 to 14 10 times
+ * @c: element
+ * Result: print 0 to 14
+ * ?
+ */
+void more_numbers(void)
+{
+	for (i = 0,i < 10, i++)
+	{
+		for (c == 48,c <= 62, c++)\
+		{
+			putchar(c);
+		}
+		putchar("/n");
+	}}
