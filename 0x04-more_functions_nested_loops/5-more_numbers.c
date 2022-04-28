7ea281bcf8e9848e6d4ad55cde20261344a56bb5
shirobite <kibromh45@gmail.com>
2022-04-28 16:29:27 +0300

    print numbers to 14

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,17 +1,27 @@
-#include <main.h>
+#include "holberton.h"
+
 /**
- * more_numbers - prints the numbers 0 to 14 10 times
- * @c: element
- * Result: print 0 to 14
- * ?
+ * more_numbers - Print 10x the numbers 0-14
+ *
+ * Return: 10x the numbers 0-14
  */
+
 void more_numbers(void)
 {
-	for (i = 0,i < 10, i++)
+	int a, b;
+
+	for (a = 0; a < 10; a++)
 	{
-		for (c == 48,c <= 62, c++)\
+		for (b = 0; b <= 14; b++)
 		{
-			_putchar(c);
+			if (b > 9)
+			{
+				_putchar((b / 10) + '0');
+			}
+
+			_putchar((b % 10) + '0');
 		}
-		_putchar("\n");
-	}}
+
+		_putchar('\n');
+	}
+}
