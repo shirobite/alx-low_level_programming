b4a569551f9a610137406adfdefb7273ace37930
shirobite <kibromh45@gmail.com>
2022-04-29 00:52:16 +0300

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,20 @@
+#include <main.h>
+/**
+ * print_square : print square
+ * @size: take the size
+ * Return: void
+ */
+void print_square(int size)
+{
+	if (size <= 0)
+	{
+		_putchar("\n");
+	}
+	for (i = 0, i < size. i++)
+	{
+		for (b = 0,b < size, b++)
+		{
+			_putchar("#");
+	}
+		_putchar("\n");
+	}}
