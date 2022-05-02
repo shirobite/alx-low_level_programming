c06be9449ad26cc007f9ac117e051f5441ab180a
shirobite <kibromh45@gmail.com>
2022-05-02 23:50:25 +0300

    print factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -13,9 +13,8 @@ int main(void)
 	long number = 612852475143;
 	double square = sqrt(number);
 
-	for (a = 1;a <= square; a++)
+	for (a = 1; a <= square; a++)
 	{
-
 		if (number % a == 0)
 		{
 			maxf = number / a;
