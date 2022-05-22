375b1f23fea3a57c1970ddbd29e6b12ba5322e31
shirobite <kibromh45@gmail.com>
2022-05-22 22:22:09 +0300

    dubgg

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,80 +1,51 @@
 #include "dog.h"
-
-
+#include <stdlib.h>
+#include <stdio.h>
 /**
-*_strlen - returns length of
-*a string
-*@str: string to be counted
-*Return: returns length of string
-*/
-int _strlen(char *str)
-{
-int len = 0;
-while (str)
-len++;
-
-return (len);
-}
-
-
-/**
-*_strcopy - copy string pointed by src
-*into dest variable
-*@dest:buffer storing string copy
-*@src: buffer storing string to copy
-*Return:returns copied string
-*/
-char *_strcopy(char *dest, char *src)
-{
-int index = 0;
-
-for (; src[index] ; index++)
-dest[index] = src[index];
-
-dest[index] = '\0';
-return (dest);
-}
-
-
-
-
-/**
-*new_dog - creates a new dog
-*@name: name of new dog
-*@age: age of new dog
-*@owner: owner of new dog
-*Return: returns NULL in case
-*of failure
-*/
+ *new_dog - creates a new dog of type dog_t
+ *@name:name
+ *@age:age
+ *@owner:owner
+ *Return:pointer to a new dog
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-dog_t *doggo;
+	int i, j, z;
+	dog_t *newd;
 
-if (name == NULL || age < 0 || owner == NULL)
-return (NULL);
-
-doggo = malloc(sizeof(dog_t));
-if (doggo == NULL)
-return (NULL);
-
-doggo->name = malloc(sizeof(char) * (_strlen(name) + 1));
-if (doggo->name == NULL)
-{
-free(doggo);
-return (NULL);
-}
-
-doggo->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
-if (doggo->owner == NULL)
-{
-free(doggo->name);
-free(doggo);
-return (NULL);
-}
-
-doggo->name = _strcopy(doggo->name, name);
-doggo->age = age;
-doggo->owner = _strcopy(doggo->owner, owner);
-
-return (doggo);
+	for (i = 0; name[i] != '\0'; i++)
+		;
+	for (j = 0; owner[j] != '\0'; j++)
+		;
+	newd = malloc(sizeof(dog_t));
+	if (newd == NULL)
+	{
+		free(newd);
+		return (NULL);
+	}
+	newd->name = malloc(i * sizeof(newd->name));
+	if (newd->name == NULL)
+	{
+		free(newd->name);
+		free(newd);
+		return (NULL);
+	}
+	for (z = 0; z <= i; z++)
+	{
+		(*newd).name[z] = name[z];
+	}
+	(*newd).age = age;
+	newd->owner = malloc(j * sizeof(newd->owner));
+	if (newd->owner == NULL)
+	{
+		free(newd->name);
+		free(newd->owner);
+		free(newd);
+		return (NULL);
+	}
+	for (z = 0; z <= j; z++)
+	{
+		newd->owner[z] = owner[z];
+	}
+	return (newd);
 }
