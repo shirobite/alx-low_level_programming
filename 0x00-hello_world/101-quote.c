b3ccf3242df858537ce5368a09d291568d17d1f8
shirobite <kibromh45@gmail.com>
2022-10-04 19:10:38 +0300

    print a line

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 1
+ */
+int main(void)
+{
+	putchar(""and that piece of art is useful" - Dora Korpar, 2015-10-19");
+	return 1;
+}
+
